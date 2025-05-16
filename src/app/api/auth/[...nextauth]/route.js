4ddca5ae57359d4d00744e9c560d9bfe87f1f6ee
amitpatel-wstf <amit.patel@thewasserstoff.com>
2025-05-16 23:59:12 +0530

    added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,46 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            "openid profile email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
+        },
+      },
     }),
   ],
-});
+  callbacks: {
+    // JWT callback to include access token and other details
+    async jwt({ token, account }) {
+      if (account) {
+        console.log("Inside JWT Authentication");
+        // Store the access token and other properties in the token object
+        token.accessToken = account.access_token;
+        if (account.email_verified) {
+          token.email_verified = account.email_verified;
+        }
+      }
+      return token;
+    },
+
+    // Session callback to pass access token to the session object
+    async session({ session, token }) {
+      console.log("Inside session Authentication", { token, session });
+
+      // Attach the access token to the session
+      if (token.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { getSession } from "next-auth/react";
 
-export default function Home() {
-  return <h1>Heading</h1>;
+export default async function Home() {
+  const session = await getSession()
+  // const { data: session, status } = useSession();
+
+  // Check if session exists
+  if (session === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (!session) {
+    return <div>Please sign in</div>;
+  }
+
+  const accessToken = session?.accessToken;
+
+  console.log("Access Token:", session);
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>Access Token: {accessToken}</p>
+    </div>
+  );
 }
